567c4147344237a7bf84d415dc9305833359744b
Abhishek Unde <126048933+Abhishek1380@users.noreply.github.com>
2023-05-04 01:02:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+function showCoupon() {
+    document.getElementById('coupons').style.visibility = 'visible';
+    document.getElementById('main').style.opacity = '0.7';
+}
+function closeCoupon() {
+    document.getElementById('coupons').style.visibility = 'hidden';
+    document.getElementById('main').style.opacity = '3';
+}
